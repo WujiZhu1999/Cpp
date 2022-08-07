15d8ccae295f7655a92cfdbd9170bed4dc9d7b22
wujizhu1999 <wujizhu1999@outlook.com>
2022-08-07 19:53:25 +1000

    test primitive update

--- 01_Basics/03_inplace_experiment.cpp
+++ 01_Basics/03_inplace_experiment.cpp
@@ -8,8 +8,11 @@ using namespace std;
 
 void test_inplace() {
     int x = 1;
+    int y = 1;
     cout << x << " " << &x << endl;
     x += 1;
+    cout << x << " " << &x << endl;
+    x = y + 1;
     cout << x << " " << &x << endl;
     x = x + 1;
     cout << x << " " << &x << endl;
